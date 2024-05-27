c1da6a80fab92c670c62ea763e71aeef1c7b7425
Asad2730 <asadsajjad777@gmail.com>
2024-05-27 22:53:39 +0500

    ...

--- handler/Book.go
+++ handler/Book.go
@@ -1,10 +1,24 @@
 package handler
 
 import (
+	"strconv"
+
 	"github.com/Asad2730/LibrarySystem/model"
 	"github.com/Asad2730/LibrarySystem/repo"
 	"github.com/gin-gonic/gin"
 )
+
+func stringToUint(str string) uint {
+	// Convert the string to uint
+	num, err := strconv.ParseUint(str, 10, 0)
+	if err != nil {
+
+		return 0
+	}
+
+	// Return the uint value
+	return uint(num)
+}
 
 func CreateBook(c *gin.Context) {
 	var book model.Book
@@ -27,6 +41,22 @@ func GetBooks(c *gin.Context) {
 	res, err := repo.GetBooks(&book)
 	if err != nil {
 		c.JSON(401, err.Error())
+		return
+	}
+
+	c.JSON(200, res)
+
+}
+
+func GetBook(c *gin.Context) {
+	id := c.Param("id")
+	uid := stringToUint(id)
+	if uid == 0 {
+		c.JSON(401, "Id type missMacth")
+	}
+	res, err := repo.GetBook(uid)
+	if err != nil {
+		c.JSON(402, err.Error())
 		return
 	}
 
--- repo/book.go
+++ repo/book.go
@@ -3,6 +3,7 @@ package repo
 import (
 	"github.com/Asad2730/LibrarySystem/internal"
 	"github.com/Asad2730/LibrarySystem/model"
+	"gorm.io/gorm"
 )
 
 func CreateBook(book *model.Book) error {
@@ -15,10 +16,29 @@ func CreateBook(book *model.Book) error {
 }
 
 func GetBooks(book *model.Book) (books *[]model.Book, err error) {
-
+	// condition := model.Book{
+	// 	Model: gorm.Model{
+	// 		DeletedAt: ,
+	// 	},
+	// }
 	if err := internal.Db.Find(book).Error; err != nil {
 		return nil, err
 	}
 
 	return books, nil
+}
+
+func GetBook(id uint) (book *model.Book, err error) {
+
+	condition := model.Book{
+		Model: gorm.Model{
+			ID: id,
+			//	DeletedAt:,
+		},
+	}
+	if err := internal.Db.First(&book, condition).Error; err != nil {
+		return nil, err
+	}
+
+	return book, nil
 }
